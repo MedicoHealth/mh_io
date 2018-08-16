c9b242c7a1e159a452b7668d772e025a07e8f802
David <david.udovic@gmail.com>
2018-08-16 16:06:42 +0200

    popravljeni vsi .pdf in .php
    Former-commit-id: 6ff719aab1670761f851ff177b91d7741ce8054d

--- includes/footer.php
+++ includes/footer.php
@@ -2,8 +2,8 @@
 	<a href="/"><img src="/img/logo-footer.png" id="footerlogo" alt="footer logo" /></a>
 		<div id="footerline">
 			<a class="altlink" href="mailto:info@medicohealth.io"><div class="footerlink">info@medicohealth.io</div></a>
-			<a class="altlink" target="_blank" href="/documents/privacy_policy.pdf"><div class="footerlink">Privacy</div></a>
-			<a class="altlink" target="_blank" href="/documents/TOU_site.pdf" ><div class="footerlink">Terms of use</div></a>
+			<a class="altlink" target="_blank" href="/documents/privacy_policy"><div class="footerlink">Privacy</div></a>
+			<a class="altlink" target="_blank" href="/documents/TOU_site" ><div class="footerlink">Terms of use</div></a>
 			<br />
 			<a class="altlink" href="#gotohome"><img src="/img/topArrow.png" alt="to the top"/><br />Back to top<br /></a>
 		</div>
